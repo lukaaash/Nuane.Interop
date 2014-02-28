f4956f842d65dd3d4d4c7d3ec0a9a97e5db86629
Lukas Pokorny <lukas.pokorny@rebex.net>
2014-03-01 00:21:03 +0100

    Merged NativeLibrary.cs and NativeLibraryMisc.cs into a single file to make it easier to include it in other projects

--- Nuane.Interop/NativeLibrary.cs
+++ Nuane.Interop/NativeLibrary.cs
@@ -393,4 +393,117 @@ namespace Nuane.Interop
 		//}
 	}
 
+	/// <summary>
+	/// Represents an error that occured while loading an unmanaged library or getting a function pointer or delegate.
+	/// </summary>
+	public class NativeLibraryException : Exception
+	{
+		/// <summary>
+		/// Creates a new instance of <see cref="NativeLibraryException"/>.
+		/// </summary>
+		public NativeLibraryException()
+		{
+		}
+
+		/// <summary>
+		/// Creates a new instance of <see cref="NativeLibraryException"/> with the specified error message.
+		/// </summary>
+		/// <param name="message">Error message.</param>
+		public NativeLibraryException(string message)
+			: base(message)
+		{
+		}
+
+		/// <summary>
+		/// Creates a new instance of <see cref="NativeLibraryException"/> with the specified error message and inner exception.
+		/// </summary>
+		/// <param name="message">Error message.</param>
+		/// <param name="innerException">Inner exception.</param>
+		public NativeLibraryException(string message, Exception innerException)
+			: base(message, innerException)
+		{
+		}
+	}
+
+	/// <summary>
+	/// Specifies options for <see cref="NativeLibrary.Load(name, NativeLibraryLoadOptions)"/>
+	/// and <see cref="NativeLibrary.TryLoad(name, NativeLibraryLoadOptions, NativeLibrary)"/>
+	/// </summary>
+	[Flags]
+	public enum NativeLibraryLoadOptions
+	{
+		/// <summary>
+		/// No options.
+		/// </summary>
+		None = 0,
+
+		/// <summary>
+		/// Look for a library with a suitable name in the assembly directory.
+		/// </summary>
+		SearchAssemblyDirectory = 1,
+
+		/// <summary>
+		/// Look for a library with a suitable name in the executable directory.
+		/// </summary>
+		SearchExecutableDirectory = 2,
+
+		/// <summary>
+		/// Look for a library with a suitable name in the current directory.
+		/// </summary>
+		SearchCurrentDirectory = 4,
+
+		/// <summary>
+		/// Look for a library with a suitable name in the assembly directory, executable directory, current directory and system directories.
+		/// </summary>
+		SearchAll = SearchAssemblyDirectory | SearchExecutableDirectory | SearchCurrentDirectory,
+	}
+
+	/// <summary>
+	/// Native methods for dynamic library loading.
+	/// </summary>
+	internal static class NativeMethods
+	{
+		#region Windows
+
+		//HMODULE LoadLibrary(LPCTSTR lpFileName)
+		[DllImport("kernel32", SetLastError = true, CharSet = CharSet.Unicode)]
+		public static extern IntPtr LoadLibrary(string lpFileName);
+
+		//FARPROC GetProcAddress(HMODULE hModule, LPCSTR lpProcName)
+		[DllImport("kernel32", SetLastError = true, CharSet = CharSet.Ansi)]
+		public static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+
+		//BOOL FreeLibrary(HMODULE hModule);
+		[DllImport("kernel32", SetLastError = true)]
+		public static extern int FreeLibrary(IntPtr hModule);
+
+		#endregion
+
+		#region Linux, *BSD, Mac OS X, Solaris, etc.
+
+		// resolve all undefined symbols in the library
+		public const int RTLD_NOW = 2;
+
+		// make symbols defined by this library available for symbol resolution of subsequently loaded libraries
+		public const int RTLD_GLOBAL = 0x100;
+
+		//void *dlopen(const char *filename, int flag)
+		[DllImport("libdl")]
+		public static extern IntPtr dlopen(string filename, int flag);
+
+		//char *dlerror(void)
+		[DllImport("libdl")]
+		public static extern IntPtr dlerror();
+
+		//void *dlsym(void *handle, const char *symbol)
+		[DllImport("libdl")]
+		public static extern IntPtr dlsym(IntPtr handle, string symbol);
+
+		//int dlclose(void *handle)
+		[DllImport("libdl")]
+		public static extern int dlclose(IntPtr handle);
+
+		#endregion
+	}
+
 }
